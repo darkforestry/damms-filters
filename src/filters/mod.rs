cbcd81e8691f6b4763fc716c59bcbe4310cff518
0xKitsune <0xKitsune@protonmail.com>
2023-02-04 13:11:22 -0500

    removed main, stripped dependencies from filters/mod.rs

--- src/filters/mod.rs
+++ src/filters/mod.rs
@@ -1,14 +1,4 @@
 pub mod blacklist;
+pub mod mechanics;
 pub mod value;
-
-use async_trait::async_trait;
-use cfmms::dex::Dex;
-use cfmms::errors::CFMMError;
-use cfmms::pool::{Pool, UniswapV2Pool, UniswapV3Pool};
-use ethers::providers::Middleware;
-use ethers::types::H160;
-use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
-use num_bigfloat::BigFloat;
-use std::collections::HashMap;
-use std::sync::Mutex;
-use std::{collections::HashSet, sync::Arc};
+pub mod volume;
