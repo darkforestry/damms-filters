6bcbe06ecbaabc7db6a6eab2e2953a0e458fa1e5
0xKitsune <0xKitsune@protonmail.com>
2023-03-30 21:59:49 -0400

    bumped to damms 0.2.0 and fixed compilation errors

--- src/filters/address.rs
+++ src/filters/address.rs
@@ -1,103 +1,69 @@
 use std::collections::HashSet;
 
-use damms::pool::Pool;
+use damms::amm::{AutomatedMarketMaker, AMM};
 use ethers::types::H160;
 
-//Filters out pools where the blacklisted address is the token_a address or token_b address
-pub fn filter_blacklisted_tokens(pools: Vec<Pool>, blacklisted_addresses: Vec<H160>) -> Vec<Pool> {
+//Filters out AMMs that contain a blacklisted token
+pub fn filter_blacklisted_tokens(amms: Vec<AMM>, blacklisted_addresses: Vec<H160>) -> Vec<AMM> {
     let mut filtered_pools = vec![];
     let blacklist: HashSet<H160> = blacklisted_addresses.into_iter().collect();
 
-    for pool in pools {
-        let mut blacklisted_token_in_pool = false;
-        for token in get_tokens_from_pool(&pool) {
+    for amm in amms {
+        let mut blacklisted_token_in_amm = false;
+        for token in amm.tokens() {
             if blacklist.contains(&token) {
-                blacklisted_token_in_pool = true;
+                blacklisted_token_in_amm = true;
+                break;
             }
         }
 
-        if !blacklisted_token_in_pool {
-            filtered_pools.push(pool);
+        if !blacklisted_token_in_amm {
+            filtered_pools.push(amm);
         }
     }
 
     filtered_pools
 }
 
-//Filters out pools where the blacklisted address is the pair address
-pub fn filter_blacklisted_pools(pools: Vec<Pool>, blacklisted_addresses: Vec<H160>) -> Vec<Pool> {
-    let mut filtered_pools = vec![];
+//Filters out AMMs where the AMM address is a blacklisted address
+pub fn filter_blacklisted_amms(amms: Vec<AMM>, blacklisted_addresses: Vec<H160>) -> Vec<AMM> {
+    let mut filtered_amms = vec![];
     let blacklist: HashSet<H160> = blacklisted_addresses.into_iter().collect();
 
-    for pool in pools {
-        if !blacklist.contains(&pool.address()) {
-            filtered_pools.push(pool);
+    for amm in amms {
+        if !blacklist.contains(&amm.address()) {
+            filtered_amms.push(amm);
         }
     }
 
-    filtered_pools
+    filtered_amms
 }
 
-//Filters out pools where the blacklisted address is the pair address, token_a address or token_b address
-pub fn filter_blacklisted_addresses(
-    pools: Vec<Pool>,
-    blacklisted_addresses: Vec<H160>,
-) -> Vec<Pool> {
-    let mut filtered_pools = vec![];
+//Filters out AMMs where AMM address or any tokens in the AMM are in the blacklist
+pub fn filter_blacklisted_addresses(amms: Vec<AMM>, blacklisted_addresses: Vec<H160>) -> Vec<AMM> {
+    let mut filtered_amms = vec![];
     let blacklist: HashSet<H160> = blacklisted_addresses.into_iter().collect();
 
-    for pool in pools {
-        let mut blacklisted_address_in_pool = false;
-        for token in get_tokens_from_pool(&pool) {
+    for amm in amms {
+        let mut blacklisted_address_in_amm = false;
+
+        //check if any of the tokens are on the blacklisted address
+        for token in amm.tokens() {
             if blacklist.contains(&token) {
-                blacklisted_address_in_pool = true;
+                blacklisted_address_in_amm = true;
             }
         }
 
-        if blacklist.contains(&pool.address()) {
-            blacklisted_address_in_pool = true;
+        //Check if the amm address is blacklisted
+        if blacklist.contains(&amm.address()) {
+            blacklisted_address_in_amm = true;
         }
 
-        if !blacklisted_address_in_pool {
-            filtered_pools.push(pool);
-        }
-    }
-
-    filtered_pools
-}
-
-pub fn get_tokens_from_pool(pool: &Pool) -> Vec<H160> {
-    match pool {
-        Pool::UniswapV2(pool) => {
-            vec![pool.token_a, pool.token_b]
-        }
-        Pool::UniswapV3(pool) => {
-            vec![pool.token_a, pool.token_b]
+        //If there are no blacklisted addresses, add the amm to the filtered amms
+        if !blacklisted_address_in_amm {
+            filtered_amms.push(amm);
         }
     }
+
+    filtered_amms
 }
-
-// //Filters only pools that contain tokens from https://tokenlists.org/
-// pub async fn filter_token_list_tokens(pools: Vec<Pool>) -> Result<Vec<Pool>, reqwest::Error> {
-//     let mut filtered_pools = vec![];
-
-//     //There is a better way to do this but this is fine and it only runs once
-//     //This is also kind of hacky but it will work
-//     //We get the json as a string then extract all of the addresses using regex, then once we have accumulated all of the addresses
-//     //We can filter the list
-//     let token_list_endpoints = vec!["", ""];
-
-//     let mut token_list_addresses = vec![];
-
-//     for endpoint in token_list_endpoints {
-//         let resp = reqwest::get(endpoint).await?.json::<String>().await?;
-//         let pattern = regex::Regex::new(r"\d+").unwrap();
-//         // iterate over all matches
-//         let endpoint_addresses = pattern
-//             .find_iter(&resp)
-//             .filter_map(|address| address.as_str().parse::<H160>().ok())
-//             .collect::<Vec<H160>>();
-
-//     }
-//     Ok(filtered_pools)
-// }
--- src/filters/value.rs
+++ src/filters/value.rs
@@ -1,6 +1,9 @@
 use std::sync::Arc;
 
-use damms::{dex::Dex, errors::CFMMError, pool::Pool};
+use damms::{
+    amm::{factory::Factory, AutomatedMarketMaker, AMM},
+    errors::DAMMError,
+};
 use ethers::{
     providers::Middleware,
     types::{H160, U256},
@@ -12,24 +15,24 @@ use crate::batch_requests;
 pub const U256_10_POW_18: U256 = U256([1000000000000000000, 0, 0, 0]);
 pub const U256_10_POW_6: U256 = U256([1000000, 0, 0, 0]);
 
-//Filter that removes pools with that contain less than a specified usd value
-pub async fn filter_pools_below_usd_threshold<M: Middleware>(
-    pools: Vec<Pool>,
-    dexes: &[Dex],
-    usd_weth_pool: Pool, //TODO: could make this f64 and just pass in price?
+//Filter that removes AMMs with that contain less than a specified usd value
+pub async fn filter_amms_below_usd_threshold<M: Middleware>(
+    amms: Vec<AMM>,
+    factories: &[Factory],
+    usd_weth_pool: AMM, //TODO: could make this f64 and just pass in price?
     usd_value_in_pool_threshold: f64, // This is the threshold where we will filter out any pool with less value than this
     weth: H160,
     weth_value_in_token_to_weth_pool_threshold: U256, //This is the threshold where we will ignore any token price < threshold during batch calls
     middleware: Arc<M>,
-) -> Result<Vec<Pool>, CFMMError<M>> {
+) -> Result<Vec<AMM>, DAMMError<M>> {
     let weth_usd_price = usd_weth_pool.calculate_price(weth)?;
 
-    //Init a new vec to hold the filtered pools
-    let mut filtered_pools = vec![];
+    //Init a new vec to hold the filtered AMMs
+    let mut filtered_amms = vec![];
 
-    let weth_values_in_pools = get_weth_values_in_pools(
-        &pools,
-        dexes,
+    let weth_values_in_pools = get_weth_values_in_amms(
+        &amms,
+        factories,
         weth,
         weth_value_in_token_to_weth_pool_threshold,
         middleware,
@@ -40,28 +43,28 @@ pub async fn filter_pools_below_usd_threshold<M: Middleware>(
         if (weth_value / U256_10_POW_18).as_u64() as f64 * weth_usd_price
             >= usd_value_in_pool_threshold
         {
-            filtered_pools.push(pools[i]);
+            filtered_amms.push(amms[i]);
         }
     }
 
-    Ok(filtered_pools)
+    Ok(filtered_amms)
 }
 
-//Filter that removes pools with that contain less than a specified weth value
+//Filter that removes AMMs with that contain less than a specified weth value
 //
-pub async fn filter_pools_below_weth_threshold<M: Middleware>(
-    pools: Vec<Pool>,
-    dexes: &[Dex],
+pub async fn filter_amms_below_weth_threshold<M: Middleware>(
+    amms: Vec<AMM>,
+    factories: &[Factory],
     weth: H160,
     weth_value_in_pool_threshold: U256, // This is the threshold where we will filter out any pool with less value than this
     weth_value_in_token_to_weth_pool_threshold: U256, //This is the threshold where we will ignore any token price < threshold during batch calls
     middleware: Arc<M>,
-) -> Result<Vec<Pool>, CFMMError<M>> {
-    let mut filtered_pools = vec![];
+) -> Result<Vec<AMM>, DAMMError<M>> {
+    let mut filtered_amms = vec![];
 
-    let weth_values_in_pools = get_weth_values_in_pools(
-        &pools,
-        dexes,
+    let weth_values_in_pools = get_weth_values_in_amms(
+        &amms,
+        factories,
         weth,
         weth_value_in_token_to_weth_pool_threshold,
         middleware,
@@ -70,20 +73,20 @@ pub async fn filter_pools_below_weth_threshold<M: Middleware>(
 
     for (i, weth_value) in weth_values_in_pools.iter().enumerate() {
         if *weth_value >= weth_value_in_pool_threshold {
-            filtered_pools.push(pools[i]);
+            filtered_amms.push(amms[i]);
         }
     }
 
-    Ok(filtered_pools)
+    Ok(filtered_amms)
 }
 
-pub async fn get_weth_values_in_pools<M: Middleware>(
-    pools: &[Pool],
-    dexes: &[Dex],
+pub async fn get_weth_values_in_amms<M: Middleware>(
+    amms: &[AMM],
+    factories: &[Factory],
     weth: H160,
     weth_value_in_token_to_weth_pool_threshold: U256,
     middleware: Arc<M>,
-) -> Result<Vec<U256>, CFMMError<M>> {
+) -> Result<Vec<U256>, DAMMError<M>> {
     let multi_progress_bar = MultiProgress::new();
     let progress_bar = multi_progress_bar.add(ProgressBar::new(0));
     progress_bar.set_style(
@@ -92,27 +95,23 @@ pub async fn get_weth_values_in_pools<M: Middleware>(
             .progress_chars("##-"),
     );
 
-    progress_bar.set_length(pools.len() as u64);
+    progress_bar.set_length(amms.len() as u64);
     progress_bar.set_message("Getting weth value in pools: ");
 
     //Init a new vec to hold the filtered pools
-    let mut aggregate_weth_values_in_pools = vec![];
+    let mut aggregate_weth_values_in_amms = vec![];
 
     let step = 300; //max batch size for this call until codesize is too large
 
     let mut idx_from = 0;
-    let mut idx_to = if step > pools.len() {
-        pools.len()
-    } else {
-        step
-    };
+    let mut idx_to = if step > amms.len() { amms.len() } else { step };
 
     //TODO: see if you can just step by the pools rather than some index
-    for _ in (0..pools.len()).step_by(step) {
-        let weth_values_in_pools =
-            batch_requests::filter_by_value::get_weth_value_in_pool_batch_request(
-                &pools[idx_from..idx_to],
-                dexes,
+    for _ in (0..amms.len()).step_by(step) {
+        let weth_values_in_amms =
+            batch_requests::filter_by_value::get_weth_value_in_amm_batch_request(
+                &amms[idx_from..idx_to],
+                factories,
                 weth,
                 weth_value_in_token_to_weth_pool_threshold,
                 middleware.clone(),
@@ -120,12 +119,12 @@ pub async fn get_weth_values_in_pools<M: Middleware>(
             .await?;
 
         //add weth values in pools to the aggregate array
-        aggregate_weth_values_in_pools.extend(weth_values_in_pools);
+        aggregate_weth_values_in_amms.extend(weth_values_in_amms);
 
         idx_from = idx_to;
 
-        if idx_to + step > pools.len() {
-            idx_to = pools.len() - 1
+        if idx_to + step > amms.len() {
+            idx_to = amms.len() - 1
         } else {
             idx_to += step;
         }
@@ -133,5 +132,5 @@ pub async fn get_weth_values_in_pools<M: Middleware>(
         progress_bar.inc(step as u64);
     }
 
-    Ok(aggregate_weth_values_in_pools)
+    Ok(aggregate_weth_values_in_amms)
 }
